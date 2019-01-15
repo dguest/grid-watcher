5676c3115d6c98710d053549c03e0a68a7de2e99
Dan Guest <dguest@cern.ch>
2019-01-15 22:23:35 +0100

    Replace file with pandamon for andrea

--- make-html.sh
+++ make-html.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cat <(echo "<table>") \
-    <(cat ~/test/exot27.json  |\
+    <(pandamon -u 'Andrea Matic' group.phys-exot*EXOT27 -j |\
           jq '[.[] | {"name": (.taskname | split(".")[4]), "fullname": .taskname, "done": .dsinfo.pctfinished, "failed": .dsinfo.pctfailed, "input":.datasets[] | select(.type == "input") | .containername}]' |\
           jq ' .[] | @html "<tr><th><label title=\"\(.input)\"> \(.name) </label></th><th style=\"background-color:rgba(0,255,0,\(.done/100))\"> \(.done) </th><th style=\"background-color:rgba(255,0,0,\(.failed/100));\"> \(.failed) </th></tr>"' -r)\
     <(echo "</table>")
