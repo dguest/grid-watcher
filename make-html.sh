975ffeba6b5ad0a22c772e741f8c5d9f25d2a83d
Dan Guest <dguest@cern.ch>
2019-01-15 22:06:02 +0100

    Add Andrea watcher

---
+++ make-html.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cat <(echo "<table>") \
+    <(pandamon -u 'Andrea Matic' group.phys-exot*EXOT27 -j  |\
+          jq '[.[] | {"name": (.taskname | split(".")[4]), "fullname": .taskname, "done": .dsinfo.pctfinished, "failed": .dsinfo.pctfailed}]' |\
+          jq ' .[] | @html "<tr><th><label title=\"\(.fullname)\"> \(.name) </label></th><th style=\"background-color:rgba(0,255,0,\(.done/100))\"> \(.done) </th><th style=\"background-color:rgba(255,0,0,\(.failed/100));\"> \(.failed) </th></tr>"' -r)\
+    <(echo "</table>")
