28535b79d4c9d4e07a8b3c301bb51a0c7c85b73a
Dan Guest <dguest@cern.ch>
2019-01-15 23:10:13 +0100

    fix repeats

--- make-html.sh
+++ make-html.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+PATH+=:$(cat ~/.ADDPATH | paste -s -d :)
+
 cat <(date) \
     <(echo "<table>") \
     <(pandamon -u 'Andrea Matic' group.phys-exot*EXOT27 -j |\
-          jq '[.[] | {"id": (.taskname | split(".")[3]), "name": (.taskname | split(".")[4]), "fullname": .taskname, "done": .dsinfo.pctfinished, "failed": .dsinfo.pctfailed, "input":.datasets[] | select(.type == "input") | .containername}]' |\
+          jq '[.[] | {"id": (.taskname | split(".")[3]), "name": (.taskname | split(".")[4]), "fullname": .taskname, "done": .dsinfo.pctfinished, "failed": .dsinfo.pctfailed, "input": [.datasets[] | select(.type == "input") | .containername][0]}]' |\
           jq ' .[] | @html "<tr><th> \(.id) </label><th><label title=\"\(.input)\"> \(.name) </label></th><th style=\"background-color:rgba(0,255,0,\(.done/100))\"> \(.done) </th><th style=\"background-color:rgba(255,0,0,\(.failed/100));\"> \(.failed) </th><th>\(.fullname)</th></tr>"' -r)\
     <(echo "</table>") > /afs/cern.ch/user/d/dguest/www/monitoring/mono-h-mc.html
