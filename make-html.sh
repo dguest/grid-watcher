6f613ee3ae517b4d28d5b9feea372fb5332f06ee
Dan Guest <dguest@cern.ch>
2019-01-15 22:56:40 +0100

    minor tweaks

--- make-html.sh
+++ make-html.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-cat <(echo "<table>") \
+cat <(date) \
+    <(echo "<table>") \
     <(pandamon -u 'Andrea Matic' group.phys-exot*EXOT27 -j |\
-          jq '[.[] | {"name": (.taskname | split(".")[4]), "fullname": .taskname, "done": .dsinfo.pctfinished, "failed": .dsinfo.pctfailed, "input":.datasets[] | select(.type == "input") | .containername}]' |\
-          jq ' .[] | @html "<tr><th><label title=\"\(.input)\"> \(.name) </label></th><th style=\"background-color:rgba(0,255,0,\(.done/100))\"> \(.done) </th><th style=\"background-color:rgba(255,0,0,\(.failed/100));\"> \(.failed) </th></tr>"' -r)\
+          jq '[.[] | {"id": (.taskname | split(".")[3]), "name": (.taskname | split(".")[4]), "fullname": .taskname, "done": .dsinfo.pctfinished, "failed": .dsinfo.pctfailed, "input":.datasets[] | select(.type == "input") | .containername}]' |\
+          jq ' .[] | @html "<tr><th> \(.id) </label><th><label title=\"\(.input)\"> \(.name) </label></th><th style=\"background-color:rgba(0,255,0,\(.done/100))\"> \(.done) </th><th style=\"background-color:rgba(255,0,0,\(.failed/100));\"> \(.failed) </th><th>\(.fullname)</th></tr>"' -r)\
     <(echo "</table>") > /afs/cern.ch/user/d/dguest/www/monitoring/mono-h-mc.html
