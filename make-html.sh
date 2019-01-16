2da75aa8b5c1e971d88b32fcd39a3696a3ed68c6
Dan Guest <dguest@cern.ch>
2019-01-16 09:48:12 +0100

    add back html

--- make-html.sh
+++ make-html.sh
@@ -7,4 +7,4 @@ cat <(echo "<b>Last Update: " $(date) "</b>") \
     <(pandamon -u 'Andrea Matic' group.phys-exot*EXOT27 -j |\
           jq '[.[] | {"id": (.taskname | split(".")[3]), "name": (.taskname | split(".")[4]), "fullname": .taskname, "done": .dsinfo.pctfinished, "failed": .dsinfo.pctfailed, "input": [.datasets[] | select(.type == "input") | .containername][0], "sites": [.datasets[].site] | join(" ")}]' |\
           jq ' .[] | @html "<tr><th> \(.id) </label><th><label title=\"\(.input)\"> \(.name) </label></th><th style=\"background-color:rgba(0,255,0,\(.done/100))\"> \(.done) </th><th style=\"background-color:rgba(255,0,0,\(.failed/100));\"> \(.failed) </th><th>\(.sites)</th></tr>"' -r)\
-    <(echo "</table>")
+    <(echo "</table>") > /afs/cern.ch/user/d/dguest/www/monitoring/mono-h-mc.html
