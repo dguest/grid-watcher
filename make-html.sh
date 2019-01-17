9c5dc7e161577bb7bdbc08f70282c35a18182148
Dan Guest <dguest@cern.ch>
2019-01-17 10:46:40 +0100

    Make me more generic

--- make-html.sh
+++ make-html.sh
@@ -1,17 +1,41 @@
 #!/usr/bin/env bash
 
-PATH=$(cat ~/.ADDPATH | paste -s -d :):${PATH}
+DR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-TMP=bobo
-OUT=bobo
+TP=$DR/temp
+OUT=temp
 
-if [[ ! -d $TMP ]] ; then mkdir $TMP ; fi
+PMON=$DR/pandamonium/pandamon
+JQ=$DR/bin/jq
 
-if [[ ! -f $TMP/rawpanda.json ]] ; then
-    pandamon -u 'Andrea Matic' group.phys-exot*EXOT27 -j > $TMP/rawpanda.json
+if [[ ! -f $JQ ]] ; then
+    JQ_URL=https://github.com/stedolan/jq/releases/download/jq-1.6/
+    JQ_VERS=jq-linux64
+    echo "getting jq"
+    wget ${JQ_URL}/${JQ_VERS}
+    mkdir -p $DR/bin
+    mv ${JQ_VERS} $JQ
+    chmod +x $JQ
 fi
 
-cat ${TMP}/rawpanda.json | jq -f refiner.jq | tee $TMP/refined.json
+# check JQ
+if ! $JQ --version &> /dev/null; then
+    # try local version
+    if type jq &> /dev/null; then
+        JQ=jq
+    else
+        echo "JQ not working" >&2
+        exit 1
+    fi
+fi
+
+if [[ ! -d $TP ]] ; then mkdir $TP ; fi
+
+if [[ ! -f $TP/rawpanda.json ]] ; then
+    $PMON -u 'Andrea Matic' group.phys-exot*EXOT27 -j > $TP/rawpanda.json
+fi
+
+cat ${TP}/rawpanda.json | $JQ -f $DR/refiner.jq | tee $TP/refined.json
 
 OUTFILE=$OUT/mono-h-mc.html
 cat <<EOF > $OUTFILE
@@ -40,7 +64,7 @@ tr:nth-child(even) {
 <table>
 <th>name</th><th colspan="3">a0L</th><th colspan="3">d0L</th><th colspan="3">e0L</th><th colspan="3">a1L</th><th colspan="3">d1L</th><th colspan="3">e1L</th><th colspan="3">a2L</th><th colspan="3">d2L</th><th colspan="3">e2L</th>
 EOF
-cat $TMP/refined.json | jq -f formatter.jq -r >> $OUTFILE
+cat $TP/refined.json | $JQ -f $DR/formatter.jq -r >> $OUTFILE
 
 cat <<EOF >> $OUTFILE
 </table>
