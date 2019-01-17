7bbda44feda3c678e856e0aa55849d6fcb6b85b6
Dan Guest <dguest@cern.ch>
2019-01-17 14:25:25 +0100

    updates

--- make-html.sh
+++ make-html.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "started job on $(date)"
+
 DR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 TP=/tmp/$(date +%F_%H-%M)
@@ -32,14 +34,24 @@ if ! $JQ --version &> /dev/null; then
     fi
 fi
 
+PY=~dguest/public/Python2.7/bin/python2.7
+if [[ -f $PY ]]; then
+    echo "using hardcoded python"
+    PMON="$PY $PMON";
+fi
+
 if [[ ! -d $TP ]] ; then mkdir $TP ; fi
 
 if [[ ! -f $TP/rawpanda.json ]] ; then
+    echo "run pandamon from $DR"
     $PMON -u 'Andrea Matic' group.phys-exot*EXOT27 -j > $TP/rawpanda.json
 fi
 
+echo "refine output"
 cat ${TP}/rawpanda.json | $JQ -f $DR/refiner.jq > $TP/refined.json
 
+
+echo "format output"
 OUTFILE=$OUT/mono-h-mc.html
 cat <<EOF > $OUTFILE
 <html>
@@ -63,7 +75,7 @@ tr:nth-child(even) {
 </style>
 </head>
 <body>
-<b>Last Update: $(date) </b>
+<b>Powered by <a href="https://github.com/dguest/grid-watcher">grid-watcher</a>. Last Update: $(date) </b>
 <table>
 <th>name</th><th colspan="3">a0L</th><th colspan="3">d0L</th><th colspan="3">e0L</th><th colspan="3">a1L</th><th colspan="3">d1L</th><th colspan="3">e1L</th><th colspan="3">a2L</th><th colspan="3">d2L</th><th colspan="3">e2L</th>
 EOF
@@ -74,3 +86,5 @@ cat <<EOF >> $OUTFILE
 </body>
 </html>
 EOF
+
+echo "done"
